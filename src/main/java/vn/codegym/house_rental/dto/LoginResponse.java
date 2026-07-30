5f57e8f83ee6af51e13c97a14b9be8d8e8d87ffe
Trung <trung@example.com>
2026-07-30 13:57:20 +0700

    update dto request, response

--- src/main/java/vn/codegym/house_rental/dto/LoginResponse.java
+++ src/main/java/vn/codegym/house_rental/dto/LoginResponse.java
@@ -1,4 +1,17 @@
 package vn.codegym.house_rental.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.catalina.Role;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginResponse {
+    private String accessToken;
+    private Role role;
+    private String username;
 }
