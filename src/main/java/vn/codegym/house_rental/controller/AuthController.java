e40d403ce5bcc4e9ce20e2f4888ce8debe447f2f
Trung <trung@example.com>
2026-07-30 16:48:28 +0700

    update login for this project

--- src/main/java/vn/codegym/house_rental/controller/AuthController.java
+++ src/main/java/vn/codegym/house_rental/controller/AuthController.java
@@ -1,4 +1,28 @@
 package vn.codegym.house_rental.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import vn.codegym.house_rental.model.User;
+import vn.codegym.house_rental.service.UserService;
+
+@Controller
+@AllArgsConstructor
 public class AuthController {
+
+    private final UserService userService;
+
+    @GetMapping("/login")
+    public String login(){
+        return "auth/login";
+    }
+
+    @PostMapping("/register")
+    public String register(User user){
+
+        userService.save(user);
+
+        return "redirect:/login";
+    }
 }
--- src/main/java/vn/codegym/house_rental/service/UserService.java
+++ src/main/java/vn/codegym/house_rental/service/UserService.java
@@ -1,7 +1,6 @@
 package vn.codegym.house_rental.service;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import vn.codegym.house_rental.model.User;
@@ -23,8 +22,8 @@ public class UserService {
         return userRepository.findById(id);
     }
 
-    public User save(User user) {
+    public void save(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        return userRepository.save(user);
+        userRepository.save(user);
     }
 }
