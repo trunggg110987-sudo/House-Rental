dd7620edd5a184a027870635888584371523a63f
Trung <trung@example.com>
2026-07-30 14:54:21 +0700

    edit userservice

--- src/main/java/vn/codegym/house_rental/service/UserService.java
+++ src/main/java/vn/codegym/house_rental/service/UserService.java
@@ -1,16 +1,19 @@
 package vn.codegym.house_rental.service;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import vn.codegym.house_rental.model.User;
 import vn.codegym.house_rental.repository.UserRepository;
 import java.util.Optional;
 
 @Service
+@AllArgsConstructor
 public class UserService {
 
-    @Autowired
     private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
 
     public Optional<User> findByUsername(String username) {
         return userRepository.findByUsername(username);
@@ -21,6 +24,7 @@ public class UserService {
     }
 
     public User save(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 }
