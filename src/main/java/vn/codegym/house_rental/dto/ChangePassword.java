64b7cbc5b2b083fafa75413922b2b5c22dd68ec7
Trung <trung@example.com>
2026-08-21 21:47:26 +0700

    use lombok

--- src/main/java/vn/codegym/house_rental/dto/ChangePassword.java
+++ src/main/java/vn/codegym/house_rental/dto/ChangePassword.java
@@ -2,7 +2,13 @@ package vn.codegym.house_rental.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.*;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ChangePassword {
 
     @NotBlank(message = "Mật khẩu hiện tại không được để trống")
@@ -15,36 +21,4 @@ public class ChangePassword {
     @NotBlank(message = "Mật khẩu xác nhận không được để trống")
     private String confirmPassword;
 
-    public ChangePassword() {
-    }
-
-    public ChangePassword(String currentPassword, String newPassword, String confirmPassword) {
-        this.currentPassword = currentPassword;
-        this.newPassword = newPassword;
-        this.confirmPassword = confirmPassword;
-    }
-
-    public String getCurrentPassword() {
-        return currentPassword;
-    }
-
-    public void setCurrentPassword(String currentPassword) {
-        this.currentPassword = currentPassword;
-    }
-
-    public String getNewPassword() {
-        return newPassword;
-    }
-
-    public void setNewPassword(String newPassword) {
-        this.newPassword = newPassword;
-    }
-
-    public String getConfirmPassword() {
-        return confirmPassword;
-    }
-
-    public void setConfirmPassword(String confirmPassword) {
-        this.confirmPassword = confirmPassword;
-    }
 }
